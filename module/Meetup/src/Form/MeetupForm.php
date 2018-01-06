54029fc09387f0a83924afb4b9f8ad5ad01ef6d2
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2018-01-06 21:13:50 +0100

    add validators and errors on input fields

--- module/Meetup/src/Form/MeetupForm.php
+++ module/Meetup/src/Form/MeetupForm.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 namespace Meetup\Form;
 
-use Zend\Filter\DateTimeSelect;
 use Zend\Form\Element;
 use Zend\Form\Form;
 use Zend\InputFilter\InputFilterProviderInterface;
+use Zend\Validator\Date;
 use Zend\Validator\StringLength;
 
 class MeetupForm extends Form implements InputFilterProviderInterface
@@ -53,7 +53,10 @@ class MeetupForm extends Form implements InputFilterProviderInterface
             ],
             'attributes' => [
                 'placeholder' => 'Meetup Date de début',
-                'class' => 'form-control'
+                'class' => 'form-control',
+                'min' => '2018-01-07',
+                'max' => '2018-12-31',
+                'step' => '1',
             ]
         ]);
         $this->add([
@@ -64,29 +67,64 @@ class MeetupForm extends Form implements InputFilterProviderInterface
             ],
             'attributes' => [
                 'placeholder' => 'Meetup Date de fin',
-                'class' => 'form-control'
+                'class' => 'form-control',
+                'step' => '1',
+                'max' => '2018-12-31',
             ]
         ]);
     }
     public function getInputFilterSpecification()
     {
+
         return [
             'title' => [
                 'required' => true,
                 'validators' => [
                     [
                         'name' => StringLength::class,
+                        'options' => [
+                            'min' => 5,
+                            'max' => 20,
+                        ],
                     ],
                 ],
             ],
             'description' => [
                 'required' => true,
+                'validators' => [
+                    [
+                       'name' => StringLength::class,
+                        'options' => [
+                            'min' => 1,
+                            'max' => 500,
+                        ],
+                    ]
+                ]
             ],
             'date_debut' => [
                 'required' => true,
+                'validators' => [
+                    [
+                        'name' => Date::class,
+                        'options' => [
+                            'format' => 'Y-m-d',
+                            'min' => date('2018-01-07'),
+                            'max' => date('2018-12-31')
+                        ],
+                    ]
+                ]
             ],
             'date_fin' => [
                 'required' => true,
+                'validators' => [
+                    [
+                        'name' => Date::class,
+                        'options' => [
+                            'format' => 'Y-m-d',
+                            'max' => date('2018-12-31')
+                        ]
+                    ]
+                ]
             ]
         ];
     }
